5a9a23cd85f27a097129d4f56cc2a4a1e9ed6c74
Danijel J <test@test.com>
2022-09-19 23:49:01 +0200

    update

--- functions.php
+++ functions.php
@@ -207,3 +207,71 @@ function wc_clean_session_cart() {
 
 
 
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
+//Remove JQuery migrate
+ 
+function remove_jquery_migrate( $scripts ) {
+	if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+		 $script = $scripts->registered['jquery'];
+	if ( $script->deps ) { 
+ 	// Check whether the script has any dependencies
+ 
+	$script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+}
+}
+}
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+// add_filter( 'wp_enqueue_scripts', 'change_default_jquery', PHP_INT_MAX );
+
+// function change_default_jquery( ){
+//     wp_dequeue_script( 'jquery');
+//     wp_deregister_script( 'jquery');   
+// }
+
+
+ /*  DISABLE GUTENBERG STYLE IN HEADER| WordPress 5.9 */
+function wps_deregister_styles() {
+    wp_dequeue_style( 'global-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'wps_deregister_styles', 100 );
+
+
+
+
+
